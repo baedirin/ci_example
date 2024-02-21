55d62928ce8c71d4b88cb2ec49cb57c8e9c8157c
Brittaney Nico Davis <davisbr2@oregonstate.edu>
2024-02-21 13:14:39 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,15 @@
 import unittest
 
+import unittest
+import task
+
+
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        expected = "Hello World"
+        self.assertEqual(task.my_func(), expected)
+
+
 if __name__ == '__main__':
     unittest.main()
